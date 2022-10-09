0099b5196ad2e4b6cdcb0466fc175c4e0c6532ed
chewzzz <chewziqing@gmail.com>
2022-10-09 23:10:40 +0800

    Add move_in_square

---
+++ day-18/move_in_square.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+my_turtle = Turtle()
+# my_turtle.shape("turtle")
+# my_turtle.color("violet")
+# # move forward
+# my_turtle.forward(100)
+# # turn right
+# my_turtle.right(90)
+for i in range(4):
+    my_turtle.forward(100)
+    my_turtle.left(90)
+
+
+
+
+
+
+# make screen that exit when we clicked
+screen = Screen()
+screen.exitonclick()
+
